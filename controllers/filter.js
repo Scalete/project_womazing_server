4034179862abebae5bf09061870ac767b97d40fa
Maxim <maximpalaguta@gmail.com>
2023-02-27 22:51:42 +0200

    refactored code and added pagination

--- controllers/filter.js
+++ controllers/filter.js
@@ -1,20 +1,39 @@
 import Category from "../models/Category.js";
 import Product from "../models/Product.js";
+import {PAGINATION_LIMIT} from "../utils/Constants.js";
+
+const onLoadCategory = async (inputCategory) => {
+
+    const foundCategory = await Category.find(inputCategory);
+    let categorySortProducts;
+
+    if (foundCategory.length !== 0) {
+        categorySortProducts = await Product.find({category: foundCategory.pop()._id}).populate('category');
+    } else {
+        categorySortProducts = await Product.find();
+    }
+
+    return categorySortProducts;
+}
+
+const onLoadPagination = (products, page) => {
+    const offset = (page - 1) * PAGINATION_LIMIT;
+    const paginatedItems = products.slice(offset, offset + PAGINATION_LIMIT);
+    const totalPages = Math.ceil(products.length / PAGINATION_LIMIT);
+
+    return {
+        page,
+        totalPages,
+        paginatedItems
+    }
+}
 
 export const onFilter = async (req, res) => {
     try {
         const categoryName = req.query.name;
-        let foundProducts;
-
-        const foundCategory = await Category.find({name: categoryName});
-
-        if (foundCategory.length !== 0) {
-            foundProducts = await Product.find({category: foundCategory.pop()._id}).populate('category');
-        } else {
-            foundProducts = await Product.find();
-        }
-
-        //код пагинации
+        const page = req.query.page || 1;
+        let foundProducts = await onLoadCategory({name: categoryName});
+        foundProducts = onLoadPagination(foundProducts, page);
 
         res.json({
             message: 'Успешная смена категории',
